f89fdc78edd1df1ffbb92af9bee1a2c66cf6357b
Ira Omelchenko <81821850+IraOmel@users.noreply.github.com>
2022-05-25 21:31:50 +0300

    lab6_models_bd_films

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('main');
 });
-
-Route::get('/about', "App\Http\Controllers\SiteController@info_page");
-Route::post('/send_form', "App\Http\Controllers\SiteController@send_info");
-Route::get('/test/{name}', "App\Http\Controllers\SiteController@test_file");
+// Route::get('/about', "App\Http\Controllers\SiteController@info_page");
+// Route::post('/send_form', "App\Http\Controllers\SiteController@send_info");
+// Route::get('/test/{name}', "App\Http\Controllers\SiteController@test_file");
 
 
 Route::get('/lab6', "App\Http\Controllers\FilmsController@show_info");
